6d6642777bef0ae71123615d7a910a9a2d6348bf
jordanfarnworth <jordanfarn23@gmail.com>
2014-01-25 17:01:38 -0700

    show function working

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 var best_day = angular.module('best_day', []);
 
+
+
+best_day.controller('bdctrl', function ($scope) {
+ 	$scope.dayList = [
+ 		{ 
+ 			day: '', 
+ 			date: '' 
+ 		}
+ 	];
+ 	var addDay = function ($scope) {
+ 		$scope.addDay.push(
+ 		{
+ 			day: $scope.newDay.day, 
+ 			date: $scope.newDay.date
+ 		});
+ 	};
+
+ });
