cb0aa5efebcf973ee259e7a10fa3a85e1997eabf
jordanfarnworth <jordanfarn23@gmail.com>
2014-01-29 21:10:39 -0700

    Got text color working

--- js/script.js
+++ js/script.js
@@ -9,12 +9,27 @@ best_day.controller('bdctrl', function ($scope) {
  			date: '11/18/1990' 
  		}
  	];
- 	$scope.addDay = function () {
+
+ 	$scope.isGood = false;
+ 	$scope.isBad = false;
+
+ 	$scope.addDayGood = function () {
  		$scope.daylist.push(
  		{
  			day: $scope.newDay.day, 
  			date: $scope.newDay.date
  		});
+ 		$scope.isGood = true;
+ 		$scope.isBad = false;
+ 	};
+ 	$scope.addDayBad = function () {
+ 		$scope.daylist.push(
+ 		{
+ 			day: $scope.newDay.day, 
+ 			date: $scope.newDay.date
+ 		});
+ 		$scope.isGood = false;
+ 		$scope.isBad = true;
  	};
  });
 
