6b729ab70226ad0bcbfa4897cb2eb59bab577089
jordanfarnworth <jordanfarn23@gmail.com>
2014-01-31 12:47:55 -0700

    got firebase wired.. finally

--- js/script.js
+++ js/script.js
@@ -1,39 +1,53 @@
-var best_day = angular.module('best_day', ["firebase"]);
+angular.module('best_day', ["firebase"]).controller("bdctrl", ["$scope", "$firebase", function($scope, $firebase) {
+    var wordJournal = new Firebase('https://quickjournal.firebaseIO.com/');
+    $scope.daylist = $firebase(wordJournal);
+
+    $scope.addDayGood = function () {
+        $scope.daylist.$add({
+            day: $scope.newDay.day,
+            date: $scope.newDay.date
+        });
+        $scope.newDay.day = "";
+        $scope.newDay.date = "";
+
+    };
+    }
+]);
 
 
-
-function bdctrl($scope, $firebase) {
+// Old code when using JUST angular (no Firebase)
+/*function bdctrl($scope, $firebase) {
 	var wordJournal = new Firebase('https://quickjournal.firebaseIO.com');
 	$scope.entries = $firebase(wordJournal);
 
-	$scope.daylist = [
- 		{ 
- 			day: 'This is an example', 
- 			date: '11/18/1990' 
- 		}
- 	];
+		$scope.daylist = [
+	 		{
+	 			day: 'This is an example',
+	 			date: '11/18/1990'
+	 		}
+	 	];
 
- 	$scope.isGood = false;
- 	$scope.isBad = false;
- 	$scope.arrayCounter = 0;
+	 	$scope.isGood = false;
+	 	$scope.isBad = false;
+	 	$scope.arrayCounter = 0;
 
- 	$scope.addDayGood = function () {
- 		$scope.daylist.push(
- 		{
- 			day: $scope.newDay.day, 
- 			date: $scope.newDay.date
- 		});
- 		$scope.isGood = true;
- 		$scope.isBad = false;
- 	};
- 	$scope.addDayBad = function () {
- 		$scope.daylist.push(
- 		{
- 			day: $scope.newDay.day, 
- 			date: $scope.newDay.date
- 		});
- 		$scope.isGood = false;
- 		$scope.isBad = true;
- 	};
-};
+	 	$scope.addDayGood = function () {
+	 		$scope.daylist.push(
+	 		{
+	 			day: $scope.newDay.day,
+	 			date: $scope.newDay.date
+	 		});
+	 		$scope.isGood = true;
+	 		$scope.isBad = false;
+	 	};
+	 	$scope.addDayBad = function () {
+	 		$scope.daylist.push(
+	 		{
+	 			day: $scope.newDay.day,
+	 			date: $scope.newDay.date
+	 		});
+	 		$scope.isGood = false;
+	 		$scope.isBad = true;
+	 	};
+}*/
 
