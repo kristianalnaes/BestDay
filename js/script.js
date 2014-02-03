af375379501fe0d06ceca37ed1e7bb9f1cbde597
jordanfarnworth <jordanfarn23@gmail.com>
2014-02-03 07:24:51 -0700

    firebase still not working

--- js/script.js
+++ js/script.js
@@ -1,18 +1,66 @@
-angular.module('best_day', ["firebase"]).controller("bdctrl", ["$scope", "$firebase", function($scope, $firebase) {
-    var wordJournal = new Firebase('https://quickjournal.firebaseIO.com/');
-    $scope.daylist = $firebase(wordJournal);
+angular.module('bestDay', ["firebase"])
+    .controller("bdctrl", ["$scope", "$firebase",
+        function($scope, $firebase) {
+            var ref = new Firebase("https://quickjournal.firebaseIO.com");
+            $scope.daylist = $firebase(ref);
+            $scope.addDayGood = function() {
+                var newPushRef = $scope.daylist.push();
 
+                newPushRef.set({
+                    desc: $scope.newDay.desc,
+                    date: $scope.newDay.date
+                });
+                $scope.newDay.desc = "";
+                $scope.newDay.date = "";
+            };
+        }
+    ]);
+
+
+
+   /* .controller("bdctrl", ["$scope", "$GreatService", "$firebase", function($scope, $firebase, $service) {
+    var ref = new Firebase('https://quickjournal.firebaseIO.com/');
+    $scope.daylist = $firebase(ref);
+    $scope.theval = 'Val ' + Math.floor(Math.random() * 1001)
+    ])};*/
+
+    /*
+   $scope.addDayGood = function () {
+       $scope.daylist.$add({
+           desc: $scope.newDay.desc,
+           date: $scope.newDay.desc,
+           value: $scope.theval
+       });
+       $scope.newDay.desc = "";
+       $scope.newDay.date = "";
+   }
+} Code works ^ */
+
+
+
+/*
     $scope.addDayGood = function () {
-        $scope.daylist.$add({
-            day: $scope.newDay.day,
-            date: $scope.newDay.date
-        });
-        $scope.newDay.day = "";
-        $scope.newDay.date = "";
 
+        $scope.daylist.$add({
+            day: $scope.day,
+            date: $scope.date,
+            value: $scope.valueA
+        });
+        $scope.day = "";
+        $scope.date = "";
     };
+    $scope.removeShow = false;
+    $scope.deleteShow = function() {
+        $scope.removeShow = true;
     }
-]);
+    $scope.deleteHide = function() {
+        $scope.removeShow = false;
+    }
+    $scope.removeDay = function() {
+        $scope.daylist.$remove($scope.day);
+    }
+    }*/
+
 
 
 // Old code when using JUST angular (no Firebase)
